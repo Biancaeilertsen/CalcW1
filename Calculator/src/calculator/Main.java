f8e1049a204f2000b6d7f8ab968cb275f7f02c17
Tonjez <tonjeeth@hotmail.com>
2018-09-12 09:44:59 +0200

    Add files via upload

---
+++ Calculator/src/calculator/Main.java
@@ -0,0 +1,30 @@
+package calculator;
+
+public class Main {
+	
+	int x;
+	int y;
+	
+	
+	public int add(int x, int y) {
+		return x + y;
+	}
+	
+	public int sub(int x, int y) {
+		return x - y;
+	}
+
+	public int mult(int x, int y) {
+		return x * y;
+	}
+
+	public int div(int x, int y) {
+		return x % y;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+
+}
