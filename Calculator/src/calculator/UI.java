6ec1bba46bf88084acdc4e6a23ffc94d1f55ab5e
Biancaeilertsen <biancaeilertsen@gmail.com>
2018-09-13 11:54:27 +0200

    added eventlisteners

--- Calculator/src/calculator/UI.java
+++ Calculator/src/calculator/UI.java
@@ -1,34 +1,165 @@
 package calculator;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class UI extends JFrame {
     private JPanel Calculator;
     private JTextArea textArea1;
-    private JButton a4Button;
-    private JButton a5Button;
     private JButton a1Button;
     private JButton a2Button;
-    private JButton a0Button;
-    private JButton cButton;
-    private JButton a6Button;
     private JButton a3Button;
-    private JButton commaButton;
-    private JButton minusButton;
-    private JButton plusButton;
-    private JButton equalsButton;
+    private JButton a4Button;
+    private JButton a5Button;
+    private JButton a6Button;
     private JButton a7Button;
     private JButton a8Button;
     private JButton a9Button;
-    private JButton multiplyButton;
+    private JButton a0Button;
+
+    private JButton cButton;
     private JButton mButton;
     private JButton bckButton;
+    private JButton commaButton;
+
+    private JButton minusButton;
+    private JButton plusButton;
+    private JButton equalsButton;
+    private JButton multiplyButton;
     private JButton percentButton;
     private JButton divideButton;
 
-    public UI(){
+    public UI() {
         add(Calculator);
         setTitle("Our Calculator");
-        setSize(400,300);
+        setSize(400, 300);
+
+        a1Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+        a2Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+        a3Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+        a4Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+        a5Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+        a6Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+        a7Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+        a8Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+        a9Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+
+        });
+        a0Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+
+        mButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+        cButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+        bckButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+        commaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+
+        minusButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+        plusButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+        equalsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+        multiplyButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+        percentButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+        divideButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+            }
+        });
+
+
+
     }
 }
