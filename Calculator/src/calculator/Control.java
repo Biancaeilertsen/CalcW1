2b52f841542f36116218010e8786e4aa3417c406
Tonje Thøgersen <tonjeeth@hotmail.com>
2018-09-13 11:39:04 +0200

    test

---
+++ Calculator/src/calculator/Control.java
@@ -0,0 +1,23 @@
+package calculator;
+
+public class Control {
+    int x;
+    int y;
+
+
+        public int add ( int x, int y){
+            return x + y;
+        }
+
+        public int sub ( int x, int y){
+            return x - y;
+        }
+
+        public int mult ( int x, int y){
+            return x * y;
+        }
+
+        public int div ( int x, int y){
+            return x % y;
+        }
+}
--- Calculator/src/calculator/Main.java
+++ Calculator/src/calculator/Main.java
@@ -13,25 +13,7 @@ public class Main {
             }
         });
 
-        int x;
-        int y;
 
-
-        /*public int add ( int x, int y){
-            return x + y;
-        }
-
-        public int sub ( int x, int y){
-            return x - y;
-        }
-
-        public int mult ( int x, int y){
-            return x * y;
-        }
-
-        public int div ( int x, int y){
-            return x % y;
-        }*/
     }
 
 }
